334dc6128712717f5089ade28a7a258c8ac000e2
Gustavo Camargo <gustavocamargo203@gmail.com>
2026-03-19 20:30:45 -0300

    Atualizacao automatica

--- meuApp/src/components/MeuComponente.tsx
+++ meuApp/src/components/MeuComponente.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
 
 
-export function MeuComponente(){
-    return(
+
+export class MeuComponente extends Component{
+
+    render (){
+
+        let nome = 'IFPR'
+
+        return(
         <View> 
             <Text>
-                Cagatora
+                Hello moto!
             </Text>
+
+            <Text style={{color : 'red', fontSize: 25, margin:15}}>
+                Primeiro mobile {nome}
+            </Text>
+
+            <Image
+
+                source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJfF98pUf53bRmy418zkV8N3OCluhtzVvjA5bNZVBHWQ&s'}}
+                style= {{width: 300, height:200}}
+            />
+
+            <Image
+
+                source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8PmTAq6tc_o1ZavZ9ych7D-iw4VHcLz8ZudhnFlAZLw&s'}}
+                style= {{width: 300, height:200}}
+            />
+
+            
         </View>
     )
+
+    }
+ 
 }
