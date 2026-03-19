6efd1ee6c123a698d62fe9133e120fdbdd817bc4
Gustavo Camargo <gustavocamargo203@gmail.com>
2026-03-19 20:04:11 -0300

    Atualizacao automatica

--- meuApp/App.tsx
+++ meuApp/App.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { MeuComponente } from './src/components/MeuComponente';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+function App(){
+  return <MeuComponente/>
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
