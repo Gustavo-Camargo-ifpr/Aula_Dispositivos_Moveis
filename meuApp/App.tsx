ab9a11d7cf9e9cf9b957a09efb7abd780d56d0c1
Gustavo-Camargo-ifpr <20241cas10010009@estudantes.ifpr.edu.br>
2026-03-26 20:41:01 -0300

    Add files via upload

--- meuApp/App.tsx
+++ meuApp/App.tsx
@@ -1,19 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { MeuComponente } from './src/components/MeuComponente';
+import React, { Component, ReactNode } from 'react';
+import { View, Text, Image } from 'react-native';
+import Mensagem from './src/components/Mensagem'
 
+interface ImagemProps{
 
-function App(){
-  return <MeuComponente/>
+  largura : Number;
+  altura : Number;
+  nome2:string;
+}
+
+class App extends Component{
+  render() {
+    let nome = 'IFPR';
+    
+
+    return (
+      <View>
+        <Text>Olá Mundo!</Text>
+        <Text>Meu segundo App!</Text>
+
+        <Text style={{ color: 'red', fontSize: 25 }}>
+          Desenvolvimento para dispositivos móveis!
+        </Text>
+
+        <Imagem largura ={300} altura={300} nome2={"Hick Gosta"}/>
+        <Mensagem/>
+        <Text style={{ fontSize: 30 }}>{nome}</Text>
+      </View>
+    );
+  }
+
+}
+
+class Imagem extends Component<ImagemProps>{
+    render(){
+      let img  =  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyvYq7cp1RLqo_6BteP3nTti80HIqe65DpxBA4v3Sv&s'
+      let img1  =  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1EjQYXCJRenpGzvnhxdbKl4J4jVdJvpcBkA&s'
+
+      
+    return(
+      <View>
+        <Image
+        source={{ uri: img }}
+        style={{ width: this.props.largura, height: this.props.altura }}
+        />
+
+        <Image
+        source={{ uri: img1 }}
+        style={{ width: this.props.largura, height: this.props.altura }}
+        />
+
+        <Text style={{ fontSize: 30 }}>{this.props.nome2}</Text>
+      </View>
+
+    )
+  }
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
